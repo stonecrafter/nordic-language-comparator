9d6e1e2adacf47df79584ef04157d9ac0ed9a970
stonecrafter <kirien.chen@gmail.com>
2018-06-30 12:50:53 -0700

    Upgrade to webpack 4

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,28 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
-  devtool: 'source-map',
+  mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new UglifyJSPlugin({
-      sourceMap: true
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        sourceMap: false,
+        uglifyOptions: {
+          compress: true,
+          ecma: 6,
+          output: {
+            comments: false
+          },
+          compress: {
+            dead_code: true,
+            drop_console: true
+          }
+        }
+      })
+    ]
+  }
 });
